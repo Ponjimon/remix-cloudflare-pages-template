7367110ed8caed41966b662af07cc827fba19f2f
Simon Nußbaumer <770632+Ponjimon@users.noreply.github.com>
2022-03-21 21:27:53 +0100

    build: update deps

--- remix.config.js
+++ remix.config.js
@@ -2,12 +2,11 @@
  * @type {import('@remix-run/dev/config').AppConfig}
  */
 module.exports = {
-  appDirectory: "app",
-  assetsBuildDirectory: "public/build",
-  publicPath: "/build/",
-  serverModuleFormat: "esm",
-  serverPlatform: "neutral",
-  serverBuildDirectory: "build",
+  appDirectory: 'app',
+  assetsBuildDirectory: 'public/build',
+  publicPath: '/build/',
+  serverBuildPath: 'build',
+  serverBuildTarget: 'cloudflare-pages',
   devServerBroadcastDelay: 1000,
-  ignoredRouteFiles: [".*"]
+  ignoredRouteFiles: ['.*'],
 };
