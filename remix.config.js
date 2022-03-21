4db5c3c5aee53ca7015f21d09f101e0bad145b59
Simon Nußbaumer <770632+Ponjimon@users.noreply.github.com>
2022-03-21 21:48:31 +0100

    build: fix build path
    needs to be index, not server.js

--- remix.config.js
+++ remix.config.js
@@ -5,7 +5,7 @@ module.exports = {
   appDirectory: 'app',
   assetsBuildDirectory: 'public/build',
   publicPath: '/build/',
-  serverBuildPath: 'build/server.js',
+  serverBuildPath: 'build/index.js',
   serverBuildTarget: 'cloudflare-pages',
   devServerBroadcastDelay: 1000,
   ignoredRouteFiles: ['.*'],
